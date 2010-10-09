90312b1b9ac05d3b2361a363104d8c4ace08926d
Almad <bugs@almad.net>
2010-10-09 13:26:15 +0200

    Fix bad signals & store default calendar in default category

--- ellaschedule/signals.py
+++ ellaschedule/signals.py
@@ -1,20 +1,50 @@
+from django.conf import settings
 from django.db.models.signals import pre_save
 
+from ella.core.models import Category
+
+from django.contrib.sites.models import Site
+from django.template.defaultfilters import slugify
+
 from models import Event, Calendar
+
+
+def get_default_category():
+    try:
+        return Category.objects.get(
+            site = Site.objects.get_current(),
+            tree_parent = None
+        )
+    except Category.DoesNotExist:
+        title = u"Default category"
+        
+        return Category.objects.create(
+            site = Site.objects.get_current(),
+            tree_path = "",
+            tree_parent = None,
+            title = title,
+            slug = slugify(title)
+        )
+
 
 def optionnal_calendar(sender, **kwargs):
     event = kwargs.pop('instance')
 
     if not isinstance(event, Event):
         return True
-    if not event.calendar:
-        try:
+    try:
+        if not event.calendar:
             calendar = Calendar._default_manager.get(name='default')
-        except Calendar.DoesNotExist:
-            calendar = Calendar(name='default', slug='default')
-            calendar.save()
-
+    except Calendar.DoesNotExist:
+        name = getattr(settings, "EVENT_DEFAULT_CALENDAR_NAME", None) or u"default"
+        calendar = Calendar(
+            name = name,
+            slug = slugify(name),
+            category = get_default_category()
+        )
+        calendar.save()
         event.calendar = calendar
+        
     return True
 
 pre_save.connect(optionnal_calendar)
